94a64a0a3b998d269640f9f9e62498c4b1241d68
Tim Dorau <scharka01@googlemail.com>
2021-08-15 21:44:52 +0200

    Add OR AND operator examples

--- Lesson1.sql
+++ Lesson1.sql
@@ -82,7 +82,7 @@ FROM orders
 LIMIT 10;
 
 /*
-Like operator
+LIKE operator
 Especially for text, WHERE = would be too specific and not return anything
 */
 
@@ -97,3 +97,83 @@ WHERE name LIKE '%one%';
 SELECT name
 FROM accounts
 WHERE name LIKE '%s';
+
+/*
+IN operator
+Allows to filer data based on several possible values
+Requires single quotation marks around non-numerical data
+*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
+
+/*
+NOT operator
+Provides the inverse results for IN, LIKE, AND Similar Operators
+*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords');
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%';
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%one%';
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%s';
+
+/*
+AND Operator
+Filter based on multiple criteria
+*/
+
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name LIKE '%s';
+
+SELECT occurred_at, gloss_qty 
+FROM orders
+WHERE gloss_qty BETWEEN 24 AND 29;
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+/*
+OR Operator
+Can be combined with other operators by using parentheses
+*/
+
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
+SELECT *
+FROM orders
+WHERE standard_qty = 0 AND (gloss_qty > 1000 OR poster_qty > 1000);
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+           AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+           AND primary_poc NOT LIKE '%eana%');
